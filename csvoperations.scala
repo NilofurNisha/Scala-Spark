cafe06346a41d0b332b7f7d33d6a3e92cebfeecd
NilofurNisha <60066877+NilofurNisha@users.noreply.github.com>
2020-10-11 17:28:05 +0530

    Update csvoperations.scala

--- csvoperations.scala
+++ csvoperations.scala
@@ -1,15 +1,18 @@
 package sparksql
 
 import org.apache.spark.SparkConf
+
+
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
-
-object lab03 {
+object lab04 {
   def main(args:Array[String])=
   {
-    val conf = new  SparkConf().setAppName("Spark-SQL-Lab01")
+    val conf = new  SparkConf().setAppName("Spark-SQL-Lab04")
                .setMaster("local")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
@@ -17,24 +20,36 @@ object lab03 {
     val sql = new SQLContext(sc)
     
     import sql.implicits._
-    
-    val schema = StructType(List(StructField("CustId",StringType,true),
-        StructField("FName",StringType,true),
-        StructField("LName",StringType,true),
-        StructField("Age",IntegerType,true),
-        StructField("Profession",StringType,true)))
-        
     val df = sql.read.format("csv")
-                .option("header",true)
-//              .option("delimiter",",")
-                .schema(schema)
                 .option("inferschema",true)
-                .load("file:/home/hduser/hive/data/custs")
-//                .toDF("column names") if no header
-    df.show()
-    
-    df.printSchema()
-//  println(df.schema)    
+                .load("file:/home/hduser/hive/data/txns")
+                .toDF("txnid","txndate","custid","amount","category","product","state","city","paymenttype")
                 
+     df.printSchema()
+     df.show()           
+     val df1 = df.filter(df("city") === "Texas" && df("paymenttype") === "credit")
+      
+     
+     val df2 = df.where("city = 'Texas' and paymenttype = 'credit'")
+     
+     val df3 = df2.select("txnid","custid","city").limit(10)
+     df3.show()
+     
+     val df4 = df.groupBy("state","city").agg(sum("amount").alias("Totalsales"),
+                 max("amount").alias("Maxamount"))
+                 
+     df4.show()            
+     
+    //Renaming column
+     val df5 = df.withColumnRenamed("paymenttype","paytype")            
+     df5.show()
+     
+    //Adding new column
+     val df6 = df.withColumn("country",lit("USA"))
+     df6.show() 
+     df6.describe().show()
+     
   }
+    
+  
 }
